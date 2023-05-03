21e382f232dfb5b4375e6576103cd7e9ecd6d100
John <jotesto@gmail.com>
2023-05-03 10:50:49 +0300

    Small fix

--- vuku-bil/layouts/last.vue
+++ vuku-bil/layouts/last.vue
@@ -16,17 +16,7 @@ export default {
 <style lang="scss" scoped>
 
 .main {
-  //min-height: 100svh;
-  //display: flex;
-  //flex-direction: column;
-  //align-items: center;
-  //justify-content: center;
-  //position: relative;
-  //
-  //.container {
-  //  padding-bottom: 65px;
-  //}
-  //
+
   .my-footer {
     width: 100%;
     display: flex;
